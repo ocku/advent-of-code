42f09682d785f46540bd408c272c8dcf2f1aeefd
ocku <feeling@lost.st>
2024-12-05 18:32:05 +0100

    day 5 - discovered tsort

---
+++ 5/p1.sh
@@ -0,0 +1,48 @@
+#!/bin/sh -eu
+
+{
+  [ $# -lt 1 ] || [ ! -f "$1" ]
+} && exit 1
+
+input="$1"
+
+print_update_if_ordered() {
+  update="$1"
+  pages="$(printf "%s" "$update" | tr ',' '|')"
+  pattern="$(printf "(%s)\\|(%s)\n" "$pages" "$pages")"
+
+  compiled_rules="$(
+    grep -E "$pattern" "$input" |
+      tr '|' ' ' |
+      tsort # life saver
+  )"
+
+  offset="-1"
+
+  for rule in $compiled_rules; do
+    current_offset="${update%%"$rule"*}"
+    current_offset="${#current_offset}"
+
+    [ "$offset" = -1 ] && {
+      offset="$current_offset"
+      continue
+    }
+
+    [ "$current_offset" -lt "$offset" ] &&
+      return
+
+    offset="$current_offset"
+  done
+
+  printf "%s\n" "$update"
+}
+
+grep ',' "$input" | while read -r update; do
+  print_update_if_ordered "$update"
+done | awk -F, '
+    {
+      print $(int((NF+1)/2))
+    }
+  ' |
+  paste -sd+ - |
+  bc # 4578
---
+++ 5/p2.sh
@@ -0,0 +1,55 @@
+#!/bin/sh -eu
+
+{
+  [ $# -lt 1 ] || [ ! -f "$1" ]
+} && exit 1
+
+input="$1"
+
+print_update_if_unordered() {
+  update="$1"
+  pages="$(printf "%s" "$update" | tr ',' '|')"
+  pattern="$(printf "(%s)\\|(%s)\n" "$pages" "$pages")"
+
+  compiled_rules="$(
+    grep -E "$pattern" "$input" |
+      tr '|' ' ' |
+      tsort # life saver
+  )"
+
+  result=""
+  offset="-1"
+  reordered=""
+
+  for rule in $compiled_rules; do
+    current_offset="${update%%"$rule"*}"
+    current_offset="${#current_offset}"
+
+    result="$result,$rule"
+
+    [ "$offset" = -1 ] && {
+      offset="$current_offset"
+      continue
+    }
+
+    [ "$current_offset" -lt "$offset" ] && {
+      reordered=1
+    }
+
+    offset="$current_offset"
+  done
+
+  [ -n "$reordered" ] &&
+    printf "%s\n" "${result#,}"
+  :
+}
+
+grep ',' "$input" | while read -r update; do
+  print_update_if_unordered "$update"
+done | awk -F, '
+    {
+      print $(int((NF+1)/2))
+    }
+  ' |
+  paste -sd+ - |
+  bc # 6179
